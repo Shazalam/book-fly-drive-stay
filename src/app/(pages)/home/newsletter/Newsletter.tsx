0683b4713aedc0aabae68b515e47bcdd9570a650
Shazalam <shazalam002786@gmail.com>
2025-06-05 08:56:29 +0530

    home page ui updated successfully

--- src/app/(pages)/home/newsletter/Newsletter.tsx
+++ src/app/(pages)/home/newsletter/Newsletter.tsx
@@ -11,14 +11,14 @@ const Newsletter = () => {
         </p>
         
         <div className="flex flex-col md:flex-row gap-4 max-w-xl mx-auto">
-          <div className="relative flex-1">
+          <div className="relative flex-1 ">
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
               <FiMail className="text-gray-400" />
             </div>
             <input 
               type="email" 
               placeholder="Your email address" 
-              className="w-full pl-10 pr-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300"
+              className="w-full pl-10 pr-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300  text-zinc-50"
             />
           </div>
           <button className="bg-white text-blue-600 hover:bg-blue-50 font-semibold py-3 px-6 rounded-lg transition duration-300">
