054cf2bea2ca7d4d076c3854068f767705b52ced
Shazalam <shazalam002786@gmail.com>
2025-08-02 01:29:51 +0530

    Update gTagManager.tsx

--- src/app/components/gTagManager.tsx
+++ src/app/components/gTagManager.tsx
@@ -6,7 +6,7 @@ const GoogleTagManager: React.FC = () => {
     <>
       {/* Google Tag - Load gtag.js */}
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=AW-17362018898"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17119997110"
         strategy="afterInteractive"
       />
 
@@ -17,7 +17,7 @@ const GoogleTagManager: React.FC = () => {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'AW-17362018898');
+          gtag('config', 'AW-17119997110');
         `}
       </Script>
     </>
